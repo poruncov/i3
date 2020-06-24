2803709e2aacbc765d23e9aafb4c18072ebdbe22
poruncov <48706308+poruncov@users.noreply.github.com>
2020-06-24 13:18:02 +0300

    Update vpn.sh

--- .config/polybar/vpn.sh
+++ .config/polybar/vpn.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-printf "VPN: " && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo OFF) | head -n 1 
+##!/bin/bash
+#printf "VPN: " && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo OFF) | head -n 1 
 
 
